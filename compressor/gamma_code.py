a674decf4d2be7acb8f786200ede2e3afafd6882
rmool <armin2saadat@gmail.com>
2021-09-16 22:42:47 +0430

    add compressor

---
+++ compressor/gamma_code.py
@@ -0,0 +1,135 @@
+class GammaCodeCompressor:
+
+    def get_compressed(self, posting_list, is_positional=True):
+        compressed = self.compress(posting_list, is_positional)
+        padded_compressed = self.set_padding(compressed)
+        return self.to_byte(padded_compressed)
+
+    def compress(self, posting_list, is_positional):
+        compressed = ""
+        last_document_number = -1
+        if is_positional:
+            for doc in posting_list:
+                # compress document number
+                document_number = doc[0]
+                compressed += self.get_gamma_code(document_number - last_document_number)
+                last_document_number = document_number
+                # compress position number
+                positions = doc[1]
+                compressed += self.get_gamma_code(len(positions))
+                last_position = -1
+                for position in positions:
+                    compressed += self.get_gamma_code(position - last_position)
+                    last_position = position
+        else:
+            for document_number in posting_list:
+                # compress document number
+                compressed += self.get_gamma_code(document_number - last_document_number)
+                last_document_number = document_number
+        return compressed
+
+    def set_padding(self, compressed):
+        padding_amount = (8 - ((len(compressed) + 3) % 8)) % 8
+        for i in range(0, padding_amount):
+            compressed += '1'
+        return self.to_3bit_binary(padding_amount) + compressed
+
+    def get_gamma_code(self, decimal_number):
+        offset = self.get_binary_code(decimal_number)[1:]
+        length = self.get_unary_code(len(offset))
+        return length + offset
+
+    def get_unary_code(self, decimal_number):
+        unary_code = ""
+        for i in range(0, decimal_number):
+            unary_code += '1'
+        unary_code += '0'
+        return unary_code
+
+    def get_binary_code(self, deciaml_number):
+        return bin(deciaml_number)[2:]
+
+    def to_3bit_binary(self, decimal_number):
+        num = decimal_number
+        reversed_binary = ""
+        for i in range(0, 3):
+            if num % 2 == 1:
+                reversed_binary += '1'
+            else:
+                reversed_binary += '0'
+            num = num // 2
+        binary = ""
+        for i in reversed(reversed_binary):
+            binary += i
+        return binary
+
+    def to_byte(self, bit_stream):
+        byte_stream = ""
+        i = 0
+        while i < len(bit_stream):
+            byte_stream += chr(int(bit_stream[i: i + 8], 2))
+            i += 8
+        return byte_stream
+
+
+class GammaCodeDecompressor:
+
+    def __init__(self):
+        self.bit_stream = ""
+        self.iterator = 3
+
+    def get_decompressed(self, byte_stream, is_positional=True):
+        self.bit_stream = ""
+        self.iterator = 3
+        self.bit_stream = self.to_bit(byte_stream)
+        return self.get_posting_list(is_positional)
+
+    def get_posting_list(self, is_positional):
+        posting_list = []
+        document_number = -1
+        document_diff = self.get_next_number()
+        if not is_positional:
+            while document_diff is not None:
+                document_number += document_diff
+                posting_list.append(document_number)
+                document_diff = self.get_next_number()
+        else:
+            while document_diff is not None:
+                document_number += document_diff
+                positions_len = self.get_next_number()
+                positions = []
+                position = -1
+                for i in range(0, positions_len):
+                    position += self.get_next_number()
+                    positions.append(position)
+                posting_list.append([document_number, positions])
+                document_diff = self.get_next_number()
+        return posting_list
+
+    def get_next_number(self):
+        length = 0
+        if self.iterator == len(self.bit_stream):
+            return None
+        while self.bit_stream[self.iterator] == '1':
+            length += 1
+            self.iterator += 1
+            if self.iterator == len(self.bit_stream):
+                return None
+        self.iterator += 1
+        offset = self.bit_stream[self.iterator: self.iterator + length]
+        self.iterator += length
+        return int('1' + offset, 2)
+
+    def to_bit(self, byte_stream):
+        bit_stream = ""
+        for byte in byte_stream:
+            bit_stream += format(ord(byte), '08b')
+        return bit_stream
+
+
+p = []
+zipper = GammaCodeCompressor()
+unzipper = GammaCodeDecompressor()
+compressed = zipper.get_compressed(p)
+print(compressed)
+print(unzipper.get_decompressed(compressed))
---
+++ compressor/variable_byte.py
@@ -0,0 +1,121 @@
+class VariableByteCompressor:
+
+    def get_compressed(self, posting_list, is_positional=True):
+        compressed = self.compress(posting_list, is_positional)
+        return self.to_byte(compressed)
+
+    def compress(self, posting_list, is_positional):
+        compressed = ""
+        last_document_number = -1
+        if is_positional:
+            for doc in posting_list:
+                # compress document number
+                document_number = doc[0]
+                compressed += self.get_variable_byte(document_number - last_document_number)
+                last_document_number = document_number
+                # compress position number
+                positions = doc[1]
+                compressed += self.get_variable_byte(len(positions))
+                last_position = -1
+                for position in positions:
+                    compressed += self.get_variable_byte(position - last_position)
+                    last_position = position
+        else:
+            for document_number in posting_list:
+                # compress document number
+                compressed += self.get_variable_byte(document_number - last_document_number)
+                last_document_number = document_number
+        return compressed
+
+    def get_variable_byte(self, decimal_number):
+        padded_binary = self.get_padded_binary(decimal_number)
+        return self.decide_msb(padded_binary)
+
+    def get_padded_binary(self, decimal_number):
+        binary = bin(decimal_number)[2:]
+        padding_number = (7 - (len(bin(decimal_number)[2:]) % 7)) % 7
+        padding = ""
+        for i in range(0, padding_number):
+            padding += '0'
+        return padding + binary
+
+    def decide_msb(self, binary):
+        i = 0
+        byte_stream = ""
+        while i < len(binary):
+            if i + 7 < len(binary):
+                byte_stream += '0' + binary[i: i + 7]
+            else:
+                byte_stream += '1' + binary[i: i + 7]
+            i += 7
+        return byte_stream
+
+    def to_byte(self, bit_stream):
+        byte_stream = ""
+        i = 0
+        while i < len(bit_stream):
+            byte_stream += chr(int(bit_stream[i: i + 8], 2))
+            i += 8
+        return byte_stream
+
+
+class VariableByteDecompressor:
+
+    def __init__(self):
+        self.bit_stream = ""
+        self.iterator = 0
+
+    def get_decompressed(self, byte_stream, is_positional=True):
+        self.bit_stream = ""
+        self.iterator = 0
+        self.bit_stream = self.to_bit(byte_stream)
+        return self.get_posting_list(is_positional)
+
+    def get_posting_list(self, is_positional):
+        posting_list = []
+        document_number = -1
+        document_diff = self.get_next_number()
+
+        if not is_positional:
+            while document_diff is not None:
+                document_number += document_diff
+                posting_list.append(document_number)
+                document_diff = self.get_next_number()
+        else:
+            while document_diff is not None:
+                document_number += document_diff
+                positions_len = self.get_next_number()
+                positions = []
+                position = -1
+                for i in range(0, positions_len):
+                    position += self.get_next_number()
+                    positions.append(position)
+                posting_list.append([document_number, positions])
+                document_diff = self.get_next_number()
+        return posting_list
+
+    def get_next_number(self):
+        binary = ""
+        if self.iterator == len(self.bit_stream):
+            return None
+        while self.bit_stream[self.iterator] == '0':
+            binary += self.bit_stream[self.iterator + 1:self.iterator + 8]
+            self.iterator += 8
+        binary += self.bit_stream[self.iterator + 1:self.iterator + 8]
+        self.iterator += 8
+        return int(binary, 2)
+
+    def to_bit(self, byte_stream):
+        bit_stream = ""
+        for byte in byte_stream:
+            bit_stream += format(ord(byte), '08b')
+        return bit_stream
+
+
+posting = []
+zipper = VariableByteCompressor()
+compressed = zipper.get_compressed(posting)
+print(compressed)
+
+unzipper = VariableByteDecompressor()
+print(unzipper.get_decompressed(compressed))
